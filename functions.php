a8cd668e9b993bb2ebaa404f2a10632f41dbad8b
Joop Laan <joop.laan@gmail.com>
2020-12-28 11:24:00 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -14,7 +14,7 @@ add_action( 'wp_enqueue_scripts', 'twentytwentyone_styles');
  */
 function valentinus_scripts() {
 	wp_enqueue_style( 'valentinus', get_stylesheet_uri() );
-	wp_enqueue_script( 'valentinus-js', get_template_directory_uri() . '/assets/js/app.js', array(), '1.0.0', true );
+	wp_enqueue_script( 'valentinus-js', get_stylesheet_directory_uri() . '/assets/js/app.js', array(), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'valentinus_scripts' );
 
